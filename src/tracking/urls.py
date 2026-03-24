1b5afefadefb8097345d6b595c7f8408fc7f7540
Oberdan Nunes <oberdan.nunes@pca.com.br>
2026-03-23 23:01:49 -0300

    Simplificação da estrutura da aplicacão com base nas telas definidas

--- src/tracking/urls.py
+++ src/tracking/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views.carriers import carriers_views
-from .views.orders import orders_views
-from .views.customers import customers_views
+from .views.orders import order_detail
 
 app_name = "tracking"
 
 urlpatterns = [
-    path("orders/", orders_views.index, name="orders"),
-    path("customers/", customers_views.index, name="customers"),
-    path("carriers/", carriers_views.index, name="carriers"),
-
+    path("orders/<int:order_id>/", order_detail.detail, name="order_detail"),
 ]
