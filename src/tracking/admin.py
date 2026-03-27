86e8cc7dc3f7db4d00c61cd5fa556c37f5be322f
Oberdan Nunes <oberdan.nunes@pca.com.br>
2026-03-26 22:03:51 -0300

    Criando models de País, Estado e Cidade

--- src/tracking/admin.py
+++ src/tracking/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
+from importlib.resources import path
 
-# Register your models here.
+from django import forms
+from django.contrib import admin
+from .models import Country, State, City
+
+admin.site.register(Country)
+   
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
+    
+    
+class CityImportForm(forms.Form):
+    file = forms.FileField(label="Arquivo CSV")
+    
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "state", "country")
+    
+    def country(self, obj):
+        return obj.state.country
+
+    country.short_description = "Country"
---
+++ src/tracking/models/__init__.py
@@ -0,0 +1,9 @@
+from .country import Country
+from .state import State
+from .city import City
+
+__all__ = [
+    "Order",
+    "Carrier",
+    "Delivery",
+]
