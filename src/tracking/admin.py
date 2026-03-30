1000a7ab7067640dd2a5938b42fba4ee6aed374c
Oberdan Nunes <oberdan.nunes@pca.com.br>
2026-03-29 21:19:14 -0300

    Criação do formulário de upload do arquivo excel para atulização dos dados dos pedidos

--- src/tracking/admin.py
+++ src/tracking/admin.py
@@ -2,18 +2,18 @@ from importlib.resources import path
 
 from django import forms
 from django.contrib import admin
-from .models import Country, State, City
+from .models import Country, State, City, Order
+from admin_extra_buttons.api import ExtraButtonsMixin, button
+from django.shortcuts import redirect
+from django.contrib.admin.templatetags.admin_urls import admin_urlname
+from django.template.response import TemplateResponse
+
 
 admin.site.register(Country)
    
 @admin.register(State)
 class StateAdmin(admin.ModelAdmin):
     list_display = ("name", "country")
-    
-    
-class CityImportForm(forms.Form):
-    file = forms.FileField(label="Arquivo CSV")
-    
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
@@ -23,3 +23,25 @@ class CityAdmin(admin.ModelAdmin):
         return obj.state.country
 
     country.short_description = "Country"
+
+class UploadForm(forms.Form):
+    docfile = forms.FileField(label='Planilha para carga')
+
+@admin.register(Order)
+class MyModelAdmin(ExtraButtonsMixin, admin.ModelAdmin):
+    @button(label='Upload planilha de carga', icon='fa-solid fa-upload', order=1)
+    def upload(self, request):
+        context = self.get_common_context(request, title='Upload')
+        if request.method == 'POST':
+            form = UploadForm(request.POST, request.FILES)
+            if form.is_valid():
+                downloaded_file = request.FILES['docfile']
+                # process file
+                ...
+                ...
+                return redirect(admin_urlname(context['opts'], 'changelist'))
+        else:
+            form = UploadForm()
+        
+        context['form'] = form
+        return TemplateResponse(request, 'admin_extra_buttons/orders_upload.html', context)
