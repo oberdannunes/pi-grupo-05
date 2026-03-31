e991ee0c3a6357398d89b4894ed9818ab4dee94e
Oberdan Nunes <oberdan.nunes@pca.com.br>
2026-03-30 21:14:59 -0300

    Inclusão da coluna delivery_date na model Orders

--- src/tracking/models/order.py
+++ src/tracking/models/order.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Order(models.Model):
     order_date = models.DateField()
+    delivery_date = models.DateField(null=True, blank=True)
     
     def __str__(self):
         return str(self.order_date)
