c5097be94e6a41d5b35906f954dcffb4b171cd44
Oberdan Nunes <oberdan.nunes@pca.com.br>
2026-03-31 19:58:57 -0300

    Ajustes na entidade order

--- src/tracking/models/order.py
+++ src/tracking/models/order.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 class Order(models.Model):
-    order_date = models.DateField()
-    delivery_date = models.DateField(null=True, blank=True)
+    nfe = models.CharField(max_length=20, verbose_name='Número NFE', default='')
+    cnpj = models.CharField(max_length=14, verbose_name='CNPJ', default='')
+    order_date = models.DateField(verbose_name='Data Pedido')
+    delivery_date = models.DateField(null=True, blank=True, verbose_name='Data Entrega')
+    status = models.CharField(max_length=20, verbose_name='Status', default='')
+    
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
     
     def __str__(self):
         return str(self.order_date)
