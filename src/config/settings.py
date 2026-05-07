3d067c05cabe116ddc5ff67e5dcf15f5a1a1ee13
Oberdan Nunes <oberdan.nunes@pca.com.br>
2026-05-06 22:04:14 -0300

    Ajuste CI/CD

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1f(47dsqr-yzn-!1ilnw5v-$10al8@qvvd*fy%hjph=eadsta7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'fastlog.bravemoss-a1b57c9b.centralus.azurecontainerapps.io']
 
 
 # Application definition
